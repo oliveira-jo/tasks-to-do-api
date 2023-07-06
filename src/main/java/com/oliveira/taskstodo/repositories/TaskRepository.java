762a1800b42c9a27359def25553d0f419b05e1d8
oliveira-jo <oliveira.jo@hotmail.com>
2023-07-06 19:31:27 -0300

    Create User and Task Repositories

---
+++ src/main/java/com/oliveira/taskstodo/repositories/TaskRepository.java
@@ -0,0 +1,28 @@
+package com.oliveira.taskstodo.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.oliveira.taskstodo.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    
+    //search for user id
+    //Spring mode
+    //Optional<Task>, anything that is null, put ampy. Function from java 8
+    List<Task> findByUser_Id(long id);
+
+    // Another Spring form, JPQL -> mix SQL and Funcions java
+    // same return List<Task> of last one
+    //@Query(value = "SELECT t FROM t WHERE t.user.id = :id") // <- : (parameter) and t from task
+    //List<Task> findByUser_Id(@Param("id")long id);
+
+    //Pure SQL
+    //@Query(value = "SELECT * FROM task t WHERE t.user.id = :id", nativeQuery = true) 
+    //List<Task> findByUser_Id(@Param("id")long id);
+
+}
---
+++ src/main/java/com/oliveira/taskstodo/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.oliveira.taskstodo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.oliveira.taskstodo.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
