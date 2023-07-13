abf259f78f26b2c748f68f9031a7340d39d1ee6f
oliveira-jo <oliveira.jo@hotmail.com>
2023-07-13 20:42:26 -0300

    Create View

---
+++ view/script.js
@@ -0,0 +1,42 @@
+const url = "http://localhost:8080/task/user/1";
+
+function hideLoader(){
+    document.getElementById("loading").style.display = "none";
+}
+
+
+function show(tasks) {
+    let tab = ` 
+        <thead> 
+            <tr>
+                <th scope="col"> # </th>
+                <th scope="col"> Descrition </th>
+                <th scope="col"> Username </th>
+                <th scope="col"> User Id </th>
+            </tr>
+        </thead>`;
+
+    for (let task of tasks) {
+        tab += `    
+            <tr>
+                <th scope="row"> ${task.id} </th>
+                <td> ${task.description} </td>
+                <td> ${task.user.username} </td>
+                <td> ${task.user.id} </td>
+            </tr>
+        `;
+    }
+    document.getElementById("tasks").innerHTML = tab;
+}
+
+async function getAPI(url){
+    const response = await fetch(url, {method: "GET"}); 
+
+    var data = await response.json();
+    console.log(data);
+    if(response)
+        hideLoader();
+    show(data);
+}
+
+getAPI(url);
